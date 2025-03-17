83321fea0e00f54641cdfb60290fc5446128bec7
akerstenl8mgmt <admin.anthony@Layer8BV.onmicrosoft.com>
2025-03-17 10:03:00 +0100

    Update side-nav.js

--- src/layouts/side-nav.js
+++ src/layouts/side-nav.js
@@ -114,48 +114,8 @@ export const SideNav = (props) => {
   //select a random sponsor image based on priority, priority 1 should be higher than priority 2 or higher
   const currentSettings = useSettings();
   const theme = currentSettings?.currentTheme?.value;
-  const sponsorimages = [
-    {
-      link: "https://rewst.io",
-      imagesrc: theme === "light" ? "/sponsors/rewst.png" : "/sponsors/rewst_dark.png",
-      priority: 1,
-    },
-    {
-      link: "https://rightofboom.com",
-      imagesrc: theme === "light" ? "/sponsors/RoB-light.svg" : "/sponsors/RoB.png",
-      priority: 1,
-    },
-    {
-      link: "https://ninjaone.com",
-      imagesrc: theme === "light" ? "/sponsors/ninjaone.png" : "/sponsors/ninjaone_white.png",
-      priority: 1,
-    },
-    {
-      link: "https://augmentt.com",
-      imagesrc: theme === "light" ? "/sponsors/augmentt-light.png" : "/sponsors/augmentt-dark.png",
-      priority: 1,
-    },
-    {
-      link: "https://huntress.com",
-      imagesrc: "/sponsors/huntress_teal.png",
-      priority: 1,
-    },
-  ];
 
-  const randomSponsorImage = () => {
-    let totalPriority = 0;
-    for (let i = 0; i < sponsorimages.length; i++) {
-      totalPriority += sponsorimages[i].priority;
-    }
-    let random = Math.floor(Math.random() * totalPriority);
-    let runningTotal = 0;
-    for (let i = 0; i < sponsorimages.length; i++) {
-      runningTotal += sponsorimages[i].priority;
-      if (random < runningTotal) {
-        return sponsorimages[i];
-      }
-    }
-  };
+
 
   const randomimg = randomSponsorImage();
   return (
@@ -214,32 +174,6 @@ export const SideNav = (props) => {
               pathname,
             })}
           </Box>
-          {profile?.clientPrincipal && (
-            <>
-              <Divider />
-              <Typography
-                color="text.secondary"
-                variant="caption"
-                sx={{ lineHeight: 4, textAlign: "center" }}
-              >
-                This application is sponsored by
-              </Typography>
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                }}
-              >
-                <img
-                  src={randomimg.imagesrc}
-                  alt="sponsor"
-                  style={{ cursor: "pointer" }}
-                  onClick={() => window.open(randomimg.link)}
-                  width={"100px"}
-                />
-              </Box>
-            </>
-          )}
         </Box>
       </Scrollbar>
     </Drawer>
